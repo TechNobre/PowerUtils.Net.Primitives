10705fcb173bd89b98f03078ff5b676fc7b16237
Nelson Nobre <technobre@gmail.com>
2023-06-04 02:02:33 +0100

    ci: force deploy

--- src/Constants/MediaTypeNames.cs
+++ src/Constants/MediaTypeNames.cs
@@ -3,13 +3,13 @@
     public static class ExtendedMediaTypeNames
     {
         public static class ProblemApplication
-        { // DONE
+        {
             public const string JSON = "application/problem+json";
             public const string XML = "application/problem+xml";
         }
 
         public static class Multipart
-        { // DONE
+        {
             public const string FORM_DATA = "multipart/form-data";
         }
     }
