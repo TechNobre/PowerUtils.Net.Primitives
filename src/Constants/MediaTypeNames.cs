76c3d644364d144c77384708339ab97a0cd45aa0
Nelson Nobre <technobre@gmail.com>
2025-08-30 13:04:06 +0100

    fix: Marked all implementations as `Obsolete`
    BREAKING CHANGE: Deprecated package

--- src/Constants/MediaTypeNames.cs
+++ src/Constants/MediaTypeNames.cs
@@ -1,16 +1,24 @@
-﻿namespace PowerUtils.Net.Constants
+﻿using System;
+
+namespace PowerUtils.Net.Constants
 {
+    [Obsolete("This package has been discontinued because it never evolved, and the code present in this package does not justify its continuation. It is preferable to implement this code directly in the project if necessary.")]
     public static class ExtendedMediaTypeNames
     {
+        [Obsolete("This package has been discontinued because it never evolved, and the code present in this package does not justify its continuation. It is preferable to implement this code directly in the project if necessary.")]
         public static class ProblemApplication
         {
+            [Obsolete("This package has been discontinued because it never evolved, and the code present in this package does not justify its continuation. It is preferable to implement this code directly in the project if necessary.")]
             public const string JSON = "application/problem+json";
+            [Obsolete("This package has been discontinued because it never evolved, and the code present in this package does not justify its continuation. It is preferable to implement this code directly in the project if necessary.")]
             public const string XML = "application/problem+xml";
         }
 
 
+        [Obsolete("This package has been discontinued because it never evolved, and the code present in this package does not justify its continuation. It is preferable to implement this code directly in the project if necessary.")]
         public static class Multipart
         {
+            [Obsolete("This package has been discontinued because it never evolved, and the code present in this package does not justify its continuation. It is preferable to implement this code directly in the project if necessary.")]
             public const string FORM_DATA = "multipart/form-data";
         }
     }
